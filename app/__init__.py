4dbeac48697a4e8922c0ff7696d199156070aff5
Adel Qalieh <aqalieh95@gmail.com>
2014-09-13 12:55:10 -0400

    Remove files when done and add comments

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, Response
 import urllib
 import pexpect
 import uuid
+import os
 
 # Create a Flask application with config
 app = Flask(__name__)
@@ -10,14 +11,26 @@ PDFLATEX_COMMAND = "pdflatex -interaction=nonstopmode -output-directory=tex {}"
 
 @app.route('/<path:url>')
 def main(url):
+    # Generate UUID for each file uploaded
     tex_uuid = uuid.uuid4()
     tex_filename = 'tex/{}'.format(str(tex_uuid))
     tex_file = urllib.urlretrieve(url, tex_filename)
 
+    # Try to render LaTeX to PDF
     tex_render = pexpect.spawn(PDFLATEX_COMMAND.format(tex_filename))
     try:
         tex_render.expect("Output written on", timeout=30)
     except pexpect.TIMEOUT:
         return "Rendering the PDF took too long, please try again!"
 
-    return Response(open(tex_filename+'.pdf'), mimetype='application/pdf')
+    # Load PDF into memory
+    pdf_filename = tex_filename + '.pdf'
+    pdf_file = open(pdf_filename)
+
+    # Remove files from filesystem
+    os.remove(tex_filename)
+    os.remove(pdf_filename)
+    os.remove(tex_filename+'.aux')
+    os.remove(tex_filename+'.log')
+
+    return Response(pdf_file, mimetype='application/pdf')
