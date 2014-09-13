355bdca1053fa9bf026fc9aed33e42bc6dbd908e
Adel Qalieh <aqalieh95@gmail.com>
2014-09-13 04:29:04 -0400

    Add LaTeX rendering route

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask, Response
-import requests
+import urllib
+import pexpect
+from werkzeug import secure_filename
+import uuid
 
 # Create a Flask application with config
 app = Flask(__name__)
 
+PDFLATEX_COMMAND = "pdflatex -interaction=nonstopmode -output-directory=tex {}"
+
 @app.route('/<path:url>')
 def main(url):
-    return Response(requests.get(url), mimetype='application/pdf')
+    tex_uuid = uuid.uuid4()
+    tex_filename = 'tex/{}'.format(str(tex_uuid))
+    tex_file = urllib.urlretrieve(url, tex_filename)
+
+    tex_render = pexpect.spawn(PDFLATEX_COMMAND.format(tex_filename))
+    try:
+        tex_render.expect("Output written on", timeout=30)
+    except pexpect.TIMEOUT:
+        return "Rendering the PDF took too long, please try again!"
+
+    return Response(open(tex_filename+'.pdf'), mimetype='application/pdf')
